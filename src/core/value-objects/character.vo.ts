436dec922322ea627d369a0a330cbb24f0b8cc2f
Heverton L. Roieski <hevertonlr@gmail.com>
2025-07-18 21:50:56 -0300

    feat(core): implement value objects for height, character and pyramid count

---
+++ src/core/value-objects/character.vo.ts
@@ -0,0 +1,17 @@
+import { PyramidDomainException } from "../exceptions/pyramid-domain.exception";
+
+export class Character {
+  private readonly value: string;
+
+  constructor(value: string) {
+    if (!value || value.length !== 1) {
+      throw PyramidDomainException.invalidCharacter();
+    }
+
+    this.value = value;
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+}
